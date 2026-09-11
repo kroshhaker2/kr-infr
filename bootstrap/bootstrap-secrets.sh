5dfaa20cf7cf3e05a408858b7c01dbf3b001a630
krosh <krosh@kroshhaker.dev>
2026-09-12 02:03:59 +0300

    feat: kube-prometheus-stack via helm

--- bootstrap/bootstrap-secrets.sh
+++ bootstrap/bootstrap-secrets.sh
@@ -7,3 +7,9 @@ kubectl create secret generic cloudflare-api-token \
   -n cert-manager \
   --from-literal=api-token="${CLOUDFLARE_API_TOKEN}" \
   --dry-run=client -o yaml | kubectl apply -f -
+
+kubectl create namespace monitoring
+kubectl create secret generic grafana-admin-credentials \
+  -n monitoring \
+  --from-literal=admin-user=admin \
+  --from-literal=admin-password="${GRAFANA_ADMIN_CREDENTIALS}"
